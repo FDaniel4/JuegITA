e1c61327dc57bb5eec1e45613e6b6e97c3ca5852
Daniel Palacio <palaciohernandezdaniel@gmail.com>
2024-10-12 14:18:33 -0600

    Inicio de sesion y registro mejorado

--- app/src/main/java/com/example/juegita/MainActivity.kt
+++ app/src/main/java/com/example/juegita/MainActivity.kt
@@ -16,36 +16,52 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Call
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
 import com.example.juegita.ui.theme.JuegITATheme
 
 class MainActivity : ComponentActivity() {
@@ -69,7 +85,10 @@ class MainActivity : ComponentActivity() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun LoginScreen(onLoginClick: () -> Unit, onRegisterClick: () -> Unit, modifier: Modifier = Modifier) {
-
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) } // Controla la visibilidad de la contraseña
+    var checkedRemember by remember { mutableStateOf(true) }
     // Background Color
     Box(
         modifier = Modifier
@@ -82,45 +101,125 @@ fun LoginScreen(onLoginClick: () -> Unit, onRegisterClick: () -> Unit, modifier:
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
+            // Icono grande circular encima de todo
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(140.dp) // Tamaño del ícono
+                    //.clip(CircleShape) // Forma circular
+                    //.background(Color.Black)
+                    .padding(6.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "User Icon",
+                    tint = Color(Color.White.value),
+                    modifier = Modifier.size(194.dp)
+                )
+            }
+            val gradientColors = listOf(Color.Cyan, Color.Magenta)
             Text(
                 text = "Iniciar sesión",
                 fontSize = 30.sp,
                 color = Color.White,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    brush = Brush.linearGradient(colors = gradientColors)
+                )
             )
 
             Spacer(modifier = Modifier.height(32.dp))
 
             // Email Input
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Correo") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.White,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.LightGray
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp), // Padding alrededor de toda la caja
+                verticalAlignment = Alignment.CenterVertically, // Alinear verticalmente al centro
+            ){
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Email",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
                 )
-            )
+                TextField(
+                    value = email,
+                    onValueChange = {email = it},
+                    label = { Text("Correo") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             // Password Input
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.White,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.LightGray
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Contraseña",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
                 )
-            )
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Contraseña") },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            Icons.Default.Info
+                        else
+                            Icons.Default.Info
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, contentDescription = null)
+                        }
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+
+            }
+            // Checkbox "Recordar contraseña"
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = checkedRemember,
+                    onCheckedChange = { checkedRemember = it },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Green,
+                        uncheckedColor = Color.White,
+                        checkmarkColor = Color.White
+                    )
+                )
+                Text(text = "Recordar contraseña", color = Color.White)
+                Text(text = "¿Olvidaste tu contraseña?", color = Color.Yellow,
+                    modifier = Modifier.padding(start = 25.dp))
+            }
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -137,14 +236,15 @@ fun LoginScreen(onLoginClick: () -> Unit, onRegisterClick: () -> Unit, modifier:
 
             // Register Button
             TextButton(onClick = onRegisterClick) {
-                Text("No estas registrado? Registrarse!", color = Color.Yellow)
+                Text("No estas registrado? Registrate!", color = Color.Yellow)
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewLoginScreen() {
     LoginScreen(onLoginClick = {}, onRegisterClick = {})
 }
+
--- app/src/main/java/com/example/juegita/RegisterScreen.kt
+++ app/src/main/java/com/example/juegita/RegisterScreen.kt
@@ -1,89 +1,276 @@
 package com.example.juegita
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.AddCircle
+import androidx.compose.material.icons.filled.Create
+import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.Info
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun RegisterScreen(onRegisterClick: () -> Unit, onLoginClick: () -> Unit, modifier: Modifier = Modifier) {
+    var email by remember { mutableStateOf("") }
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
+    var passwordVisible by remember { mutableStateOf(false) } // Controla la visibilidad de la contraseña
+    var passwordsMatch by remember { mutableStateOf(true) }
+    var checkedRemember by remember { mutableStateOf(false) }
     // Background Color
     Box(
         modifier = modifier
             .fillMaxSize()
             .background(Color(0xFF6200EA)) // Light Blue background
-            .padding(16.dp)
+            .padding(5.dp)
+
     ) {
+//        Image(
+//            painter = painterResource(R.drawable.fondo_neon),
+//            contentDescription = "Background Image",
+//            modifier = Modifier
+//                .fillMaxSize(),
+//            contentScale = ContentScale.Crop // Escala la imagen para cubrir todo el espacio
+//        )
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center,
             modifier = Modifier.fillMaxSize()
         ) {
+            // Icono grande circular encima de todo
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(140.dp) // Tamaño del ícono
+                    //.clip(CircleShape) // Forma circular
+                    //.background(Color.Black)
+                    .padding(4.dp)
+            ) {
+                Icon(
+                    painter = painterResource(R.drawable.person_add),
+                    contentDescription = "Create Icon",
+                    tint = Color(Color.White.value),
+                    modifier = Modifier.size(194.dp)
+                )
+            }
+            val gradientColors = listOf(Color.Cyan, Color.Magenta)
+
             Text(
-                text = "Registro",
+                text = "Crear nueva cuenta",
                 fontSize = 30.sp,
-                color = Color.White,
-                fontWeight = FontWeight.Bold
+                fontWeight = FontWeight.Bold,
+                style = TextStyle(
+                    brush = Brush.linearGradient(colors = gradientColors)
+                )
             )
+
 
             Spacer(modifier = Modifier.height(32.dp))
 
             // Username Input
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Username") },
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.White,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.LightGray
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp), // Padding alrededor de toda la caja
+                verticalAlignment = Alignment.CenterVertically, // Alinear verticalmente al centro
+            ){
+                Icon(
+                    imageVector = Icons.Default.AccountCircle,
+                    contentDescription = "Username",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
                 )
-            )
+                TextField(
+                    value = username,
+                    onValueChange = {username = it},
+                    label = { Text("Username") },
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             // Email Input
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Correo") },
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Email),
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.White,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.LightGray
+            Row (
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                Icon(
+                    imageVector = Icons.Default.Email,
+                    contentDescription = "Correo",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
                 )
-            )
+                TextField(
+                    value = email,
+                    onValueChange = {email = it},
+                    label = { Text("Correo") },
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+            }
 
             Spacer(modifier = Modifier.height(16.dp))
 
-            // Password Input
-            TextField(
-                value = "",
-                onValueChange = {},
-                label = { Text("Contraseña") },
-                visualTransformation = PasswordVisualTransformation(),
-                keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
-                modifier = Modifier.fillMaxWidth(),
-                colors = TextFieldDefaults.textFieldColors(
-                    containerColor = Color.White,
-                    focusedIndicatorColor = Color.Green,
-                    unfocusedIndicatorColor = Color.LightGray
+            // Campo de contraseña
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Password",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
                 )
-            )
+                TextField(
+                    value = password,
+                    onValueChange = { password = it },
+                    label = { Text("Contraseña") },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            Icons.Default.Info
+                        else
+                            Icons.Default.Info
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, contentDescription = null)
+                        }
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            // Campo de confirmación de contraseña
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Lock,
+                    contentDescription = "Confirmar contraseña",
+                    tint = Color.Black,
+                    modifier = Modifier
+                        .size(40.dp)
+                        .padding(end = 8.dp)
+                )
+                TextField(
+                    value = confirmPassword,
+                    onValueChange = {
+                        confirmPassword = it
+                        passwordsMatch = it == password // Verifica si las contraseñas coinciden
+                    },
+                    label = { Text("Confirmar Contraseña") },
+                    visualTransformation = if (passwordVisible) VisualTransformation.None else PasswordVisualTransformation(),
+                    keyboardOptions = KeyboardOptions.Default.copy(keyboardType = KeyboardType.Password),
+                    modifier = Modifier.fillMaxWidth(),
+                    trailingIcon = {
+                        val image = if (passwordVisible)
+                            Icons.Default.Info
+                        else
+                            Icons.Default.Info
+
+                        IconButton(onClick = { passwordVisible = !passwordVisible }) {
+                            Icon(imageVector = image, contentDescription = null)
+                        }
+                    },
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.White,
+                        focusedIndicatorColor = Color.Green,
+                        unfocusedIndicatorColor = Color.LightGray
+                    )
+                )
+            }
+
+            // Mensaje de error si las contraseñas no coinciden
+            if (!passwordsMatch) {
+                Text(
+                    text = "Las contraseñas no coinciden.",
+                    color = Color.Red,
+                    modifier = Modifier.padding(top = 8.dp)
+                )
+            }
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .padding(top = 6.dp)
+                    .fillMaxWidth()
+            ) {
+                Checkbox(
+                    checked = checkedRemember,
+                    onCheckedChange = { checkedRemember = it },
+                    colors = CheckboxDefaults.colors(
+                        checkedColor = Color.Green,
+                        uncheckedColor = Color.White,
+                        checkmarkColor = Color.White
+                    )
+                )
+                Text(text = "Estoy de acuerdo con los terminos y condiciones", color = Color.Yellow, fontSize = 12.sp)
+
+            }
 
             Spacer(modifier = Modifier.height(24.dp))
 
@@ -93,21 +280,22 @@ fun RegisterScreen(onRegisterClick: () -> Unit, onLoginClick: () -> Unit, modifi
                 modifier = Modifier.fillMaxWidth(),
                 colors = ButtonDefaults.buttonColors(Color(0xFFFF5722)) // Orange Button
             ) {
-                Text(text = "Registrarse", color = Color.White, fontSize = 18.sp)
+                Text(text = "Registrarse", color = Color.White, fontSize = 19.sp)
             }
 
             Spacer(modifier = Modifier.height(16.dp))
 
             // Go to Login Button
             TextButton(onClick = onLoginClick) {
-                Text("¿Ya tienes una cuenta? Iniciar sesión!", color = Color.Yellow)
+                Text("¿Ya tienes una cuenta? Inicia sesión!", color = Color.Yellow)
             }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun PreviewRegisterScreen() {
     RegisterScreen(onRegisterClick = {}, onLoginClick = {})
 }
+
